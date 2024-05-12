a200a7deb3cea8c06433e363bcac77a9a1c6c822
Coberlight <romchik2484@gmail.com>
2024-05-12 04:47:00 +0400

    первый коммит проекта демонстрации кэшированного поиска
    туду:
    - отловить исключения, бросаемых методами из других потоков (потоки составления кэша поиска и потоки самого поиска)
    - а что ещё...

---
+++ CachedFileSearch/CachedFileSearchCore.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CachedFileSearch
+{
+    internal class CachedFileSearchCore
+    {
+        public delegate void SearchLog(string message);
+        public static string MakeCache(string[] paths, int pathDepth, SearchLog logger)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < paths.Length; i++)
+            {
+                sb.Append(paths[i]);
+                ProcessFolderCache(new DirectoryInfo(paths[i]), sb, pathDepth, logger);
+            }
+            return sb.ToString();
+        }
+        public static string MakeCache(string path, int pathDepth, SearchLog logger)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(path);
+            ProcessFolderCache(new DirectoryInfo(path), sb, pathDepth, logger);
+            return sb.ToString();
+        }
+        private static void ProcessFolderCache(DirectoryInfo directoryInfo, StringBuilder sb, int pathDepth, SearchLog logger)
+        {
+            logger?.Invoke(directoryInfo.FullName);
+            FileInfo[] files = directoryInfo.GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+                sb.Append(files[i].Name);
+                sb.Append(Path.PathSeparator);
+            }
+            DirectoryInfo[] directories = directoryInfo.GetDirectories();
+            for (int i = 0; i < directories.Length; i++)
+            {
+                sb.Append(directories[i].FullName);
+                sb.Append(Path.PathSeparator);
+                if (pathDepth != 1)
+                {
+                    ProcessFolderCache(directories[i], sb, pathDepth == 0 ? 0 : pathDepth - 1, logger);
+                }
+            }
+        }
+        /// <summary>
+        /// Метод поиска файлов и папок в кэше.
+        /// logger - делегат, вызывающийся с названием текущей обрабатываемой директории для отображения информации о поиске.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="cache"></param>
+        /// <param name="logger"></param>
+        /// <returns></returns>
+        public static List<FileSystemInfo> FindMatches(string query, ref string cache, bool enableCase, SearchLog logger)
+        {
+            List<FileSystemInfo> matchedFSIs = new List<FileSystemInfo>();
+            int currentIndex = 0;
+            int cacheLength = cache.Length;
+            string currentDirectory = GetNextPath(ref cache, ref currentIndex);
+            logger?.Invoke(currentDirectory);
+            if (!enableCase) query = query.ToLower();
+            while (currentIndex < cacheLength)
+            {
+                string nextLine = GetNextPath(ref cache, ref currentIndex);
+                string nextLineProcessed = enableCase ? nextLine : nextLine.ToLower();
+                if (Path.IsPathRooted(nextLine))
+                {
+                    currentDirectory = nextLine;
+                    if (Path.GetFileName(nextLineProcessed).IndexOf(query) != -1)
+                    {
+                        matchedFSIs.Add(new DirectoryInfo(currentDirectory));
+                        logger?.Invoke(currentDirectory);
+                    }
+                } else
+                {
+                    if (nextLineProcessed.IndexOf(query) != -1)
+                    {
+                        matchedFSIs.Add(new FileInfo(Path.Combine(currentDirectory, nextLine)));
+                    }
+                }
+            }
+            return matchedFSIs;
+        }
+
+        public static string GetNextPath(ref string cache, ref int index)
+        {
+            int oldIndex = index;
+            index = cache.IndexOf(Path.PathSeparator, index);
+            return cache.Substring(oldIndex, index++ - oldIndex);
+        }
+    }
+}
---
+++ CachedFileSearch/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CachedFileSearch
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string fileCache;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBlock_MatchesList.Text = "Сквазимабзабза\nБабынбубынбубз\nБубжидубжизус\nБалдымалдыбар\nБарындылдан\nКраблатубажидан\nАркынтырыксус\nМамзиказмамаба\nАроклапежус\nФисташканегречкашрёк\nПирдырдыл\nМамзаказибуба\nПралгабрадафныр\nПакыртылтынлдан\nАкралдыштус\nШвабрадалдан\nДырындилдан\nБуксушашан\nПараншран\nДилдалдан\nБрыгылыгыла\nЧучпекжизуз\nИлкылдылдан";
+        }
+
+        private async void Button_MakeCache_Click(object sender, RoutedEventArgs e)
+        {
+            // Подготовка аргументов для метода, подготовка логгера, создание задачи
+            string location = TextBox_DirectoryLocation.Text;
+            CachedFileSearchCore.SearchLog logger = SetLogMakeCacheMessage;
+            Task<string> task;
+            // исключение возникает в другом потоке, поэтому отловить его отсюда обычными средствами не получается
+            //try
+            //{
+                task = new Task<string>(() => CachedFileSearchCore.MakeCache(new string[] { location.Trim('\"') }, 0, logger));
+            //}
+            //catch (Exception ex) { TextBlock_Info.Text = "Оп ахах неловко вышло\n" + ex.Message; return; }
+
+            // Запуск задачи с измерением времени выполнения
+            DateTime dt = DateTime.Now;
+            task.Start();
+            fileCache = await task;
+            TextBlock_Info.Text = "Кэш создан. \nВремя сканирования: " + (DateTime.Now - dt).TotalMilliseconds.ToString() + " мс";
+
+            // Показ результата
+            TextBlock_FileSystemList.Text = fileCache;
+        }
+
+        private async void Button_SearchGo_Click(object sender, RoutedEventArgs e)
+        {
+            List<FileSystemInfo> fsis;
+
+            // Подготовка аргументов для метода, подготовка логгера, создание задачи
+            string fileCache = this.fileCache;
+            string query = TextBox_Query.Text;
+            bool enableCase = (bool)CheckBox_EnableCase.IsChecked;
+            CachedFileSearchCore.SearchLog logger = SetLogSearchMessage;
+            Task<List<FileSystemInfo>> task;
+            //try
+            //{
+                task = new Task<List<FileSystemInfo>>(() => CachedFileSearchCore.FindMatches(query, ref fileCache, enableCase, logger));
+            //}
+            //catch (Exception ex) { TextBlock_Info.Text = "Оп ахах неловко вышло\n" + ex.Message; return; }
+
+            // Запуск задачи с измерением времени выполнения
+            DateTime dt = DateTime.Now;
+            task.Start();
+            fsis = await task;
+            TextBlock_Info.Text = "Поиск завершён.\nВремя поиска: " + (DateTime.Now - dt).TotalMilliseconds.ToString() + " мс\nВсего результатов: " + fsis.Count;
+
+            // Показ результата
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < fsis.Count; i++)
+            {
+                if (fsis[i] is DirectoryInfo)
+                {
+                    sb.Append("Папка: ");
+                }
+                else if (fsis[i] is FileInfo)
+                {
+                    sb.Append("Файл: ");
+                }
+                else
+                {
+                    sb.Append("Чё за хрень воще: ");
+                }
+                sb.Append(fsis[i].FullName).Append('\n');
+            }
+            TextBlock_MatchesList.Text = sb.ToString();
+        }
+        private void SetLogMakeCacheMessage(string logMessage)
+        {
+            this.Dispatcher.Invoke(() => TextBlock_Info.Text = "Обрабатываемая директория:\n" + logMessage);
+        }
+        private void SetLogSearchMessage(string logMessage)
+        {
+            this.Dispatcher.Invoke(() => TextBlock_Info.Text = "Обрабатываемая директория:\n" + logMessage);
+        }
+
+        private void Button_LoadCache_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            if (fd.FileName != null && fd.FileName != "")
+            {
+                fileCache = File.ReadAllText(fd.FileName);
+                TextBlock_FileSystemList.Text = fileCache;
+            }
+        }
+
+        private void Button_SaveCache_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.ShowDialog();
+            if (fd.FileName != null && fd.FileName != "")
+            {
+                File.WriteAllText(fd.FileName, fileCache);
+            }
+        }
+    }
+}
